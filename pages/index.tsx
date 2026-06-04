71a4fd4d4d24c42c5af185cb5a22a3758209d023
Lex Miin <hello@lexunix.me>
2026-06-04 17:38:30 +0300

    web: update info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
-import Container from '@/components/container'
-import s from '@/styles/index.module.scss'
-import Image from 'next/image'
-import profileImage from '../public/profile.png'
-import { getClient } from '@/lib/sanity-server'
-import { recentPostsQuery } from '@/lib/queries'
-import { Post } from '@/lib/types'
-import { InferGetStaticPropsType } from 'next'
-import { projects } from '@/lib/projects'
-import Link from 'next/link'
+import Container from "@/components/container";
+import s from "@/styles/index.module.scss";
+import Image from "next/image";
+import profileImage from "../public/profile.png";
+import { getClient } from "@/lib/sanity-server";
+import { recentPostsQuery } from "@/lib/queries";
+import { Post } from "@/lib/types";
+import { InferGetStaticPropsType } from "next";
+import { projects } from "@/lib/projects";
+import Link from "next/link";
 
-export default function Home({
-  posts
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <Container description="Software Engineer. Currently in Paris, France.">
+    <Container description="Software Engineer based in Kyiv, Ukraine.">
       <div className={s.info}>
-        <div className={s['img-container']}>
+        <div className={s["img-container"]}>
           <Image
             src={profileImage}
             placeholder="blur"
@@ -27,19 +25,17 @@ export default function Home({
         </div>
         <div>
           <p className={s.name}>Alexey Miin</p>
-          <p className={s.muted}>
-            Software Engineer at Uvaga Communications Agency
-          </p>
+          <p className={s.muted}>Software Engineer at Uvaga Communications Agency</p>
         </div>
       </div>
       <div className={s.about}>
-        <p>Software Engineer based in Paris, France.</p>
+        <p>Software Engineer based in Kyiv, Ukraine.</p>
       </div>
       <div className={s.content}>
         <section className={s.content}>
           <h1>Projects</h1>
           <div className={s.subcontent}>
-            {projects.slice(0, 2).map(p => (
+            {projects.slice(0, 2).map((p) => (
               <div key={p.name}>
                 <a href={p.href} target="_blank" className={s.subTitle}>
                   {p.name}
@@ -57,7 +53,7 @@ export default function Home({
         <section className={s.content}>
           <h1>Writing</h1>
           <div className={s.subcontent}>
-            {posts.map(p => (
+            {posts.map((p) => (
               <div key={p._id}>
                 <a href={`/writing/${p.slug}`} className={s.subTitle}>
                   {p.title}
@@ -74,11 +70,11 @@ export default function Home({
         </section>
       </div>
     </Container>
-  )
+  );
 }
 
 export const getStaticProps = async ({ preview = false }) => {
-  const posts: Post[] = await getClient(preview).fetch(recentPostsQuery)
+  const posts: Post[] = await getClient(preview).fetch(recentPostsQuery);
 
-  return { props: { posts } }
-}
+  return { props: { posts } };
+};
