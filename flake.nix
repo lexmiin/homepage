237f950e7daf95e5317f387067c235ee374dd72f
Lex Miin <hello@lexunix.me>
2026-07-06 14:04:43 +0300

    pnpm: update pnpm to v11

--- flake.nix
+++ flake.nix
@@ -1,46 +1,39 @@
 {
   description = "A Nix-flake-based Node.js development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ self.overlays.default ];
-            };
-          }
-        );
-    in
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     {
-      overlays.default = final: prev: rec {
-        nodejs = prev.nodejs_22;
-        yarn = (prev.yarn.override { inherit nodejs; });
+      overlays.default = final: prev: {
+        nodejs = final.nodejs_24;
+        pnpm = final.pnpm_11;
       };
+    }
+    // (flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.default
+          ];
+        };
+      in {
+        formatter = pkgs.alejandra;
 
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              nodejs
-              nodePackages.pnpm
-              yarn
-            ];
-          };
-        }
-      );
-    };
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs
+            pkgs.pnpm
+          ];
+        };
+      }
+    ));
 }
