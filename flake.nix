66b698700a1868fe59d94a16851fca3c80afc213
Lex Miin <hello@lexunix.me>
2026-07-17 13:19:02 +0300

    secrets: init fnox

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-26.05";
     flake-utils.url = "github:numtide/flake-utils";
+    fnox.url = "github:lexmiin/fnox-nix";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    fnox,
   }:
     {
       overlays.default = final: prev: {
@@ -23,6 +25,7 @@
           inherit system;
           overlays = [
             self.overlays.default
+            fnox.overlays.default
           ];
         };
       in {
@@ -32,6 +35,7 @@
           packages = [
             pkgs.nodejs
             pkgs.pnpm
+            pkgs.fnox
           ];
         };
       }
