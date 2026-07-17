d19e2fdb9591b26df785ac1ae55ec4d33f997395
Lex Miin <hello@lexunix.me>
2026-07-17 12:40:45 +0300

    writing: add copy button to code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,39 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import vercel from '@astrojs/vercel'
 
+const copyButtonTransformer = {
+  name: 'copy-button',
+  root(node) {
+    node.children = [
+      {
+        type: 'element',
+        tagName: 'div',
+        properties: { className: ['code-block'] },
+        children: [
+          ...node.children,
+          {
+            type: 'element',
+            tagName: 'button',
+            properties: {
+              className: ['code-copy-button'],
+              type: 'button',
+              ariaLive: 'polite'
+            },
+            children: [
+              {
+                type: 'element',
+                tagName: 'span',
+                properties: { className: ['visually-hidden'] },
+                children: [{ type: 'text', value: 'Copy code to clipboard' }]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
+
 export default defineConfig({
   site: 'https://lexunix.me',
   markdown: {
@@ -9,7 +42,8 @@ export default defineConfig({
       themes: {
         light: 'github-light',
         dark: 'vesper'
-      }
+      },
+      transformers: [copyButtonTransformer]
     }
   },
   integrations: [react()],
